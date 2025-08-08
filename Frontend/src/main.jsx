c2456c1f9fdf93eb87b297b532a3e8fec225c752
Ankur Sharma <ankusharmapan125@gmail.com>
2025-08-08 12:32:49 +0530

    adding routes  configurations

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom' 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastContainer theme="dark" position='top-right' autoClose={3000} closeOnClick pauseOnHover={false} />
+      <AuthContextProvider>
+      <App/>
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )

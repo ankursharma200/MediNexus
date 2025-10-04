ec2261cb1175fea61c3447cadf0aa36427fc1e07
Ankur Sharma <ankusharmapan125@gmail.com>
2025-10-04 15:29:12 +0530

    fix: Correctly use BASE_URL in useFetchData hook

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,5 +2,5 @@
 const url_production = "http://localhost:5000";
 // eslint-disable-next-line no-unused-vars
 const url_local = "http://localhost:5000"
-export const BASE_URL = url_production;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const token = localStorage.getItem('token');
--- Frontend/src/hooks/useFetchData.js
+++ Frontend/src/hooks/useFetchData.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { token } from "../config.js";
+import { token, BASE_URL } from "../config.js";
 
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
@@ -10,9 +10,13 @@ const useFetchData = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await fetch(url, {
+         const res = await fetch(`${BASE_URL}${url}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
+
+        // const res = await fetch(url, {
+        //   headers: { Authorization: `Bearer ${token}` },
+        // });
         const result = await res.json();
         //console.log("Result", result);
         if (!res.ok) {
